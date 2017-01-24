562e41e9d7e4528a37ecca75e4f220bb50117917
Pieter Zwart <pieter_dragon3@hotmail.com>
2017-01-24 16:25:49 +0100

    login page werkend met password reset

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('voorbeeld', 'ExampleController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
